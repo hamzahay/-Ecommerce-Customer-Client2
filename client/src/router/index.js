b31ae480e4c6523197b4ed0f8b04fc38e2c707ec
hamzahay <hamzah.ay124@gmail.com>
2021-02-25 13:00:47 +0700

    update 3

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -39,4 +39,12 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.name === 'Login' && localStorage.access_token) {
+    next({ name: 'Home' })
+  } else {
+    next()
+  }
+})
+
 export default router
